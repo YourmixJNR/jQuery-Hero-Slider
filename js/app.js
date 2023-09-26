095ad5e1884ae8ba836cc90f203f3a6a6341717f
Michael Victor <radmakson@gmail.com>
2023-09-26 23:46:31 +0100

    show specific data

--- js/app.js
+++ js/app.js
@@ -14,3 +14,24 @@ $(document).ready(function(){
     });
 
 });
+
+// Function to show a specific slide
+function showSlide(index) {
+    const item = jsonData[index];
+    const heroContainer = $('.hero');
+    heroContainer.empty();
+
+    const imgURL = item.backgroundImg;
+    const bigText = item.bigText;
+    const smallText = item.smallText;
+
+    const slideHTML = `
+        <div class="slide">
+            <img src="${imgURL}" alt="Slide Image">
+            <h1>${bigText}</h1>
+            <p>${smallText}</p>
+        </div>
+    `;
+
+    heroContainer.append(slideHTML);
+}
